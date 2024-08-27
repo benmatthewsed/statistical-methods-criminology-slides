f6d23c553b2d088f56a7a4014f71eefc21b28fca
Ben Matthews <benmatthewsed@gmail.com>
2024-08-27 14:53:47 +0100

    big updates to simulation, small updates to ethics

--- practical_04.R
+++ practical_04.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(clarify)
 library(broom)
 
 # see pages 58-63 at https://www.gov.scot/binaries/content/documents/govscot/publications/statistics/2020/09/simd-2020-technical-notes/documents/simd-2020-technical-notes/simd-2020-technical-notes/govscot%3Adocument/SIMD%2B2020%2Btechnical%2Bnotes.pdf
@@ -20,6 +21,12 @@ library(broom)
 
 simd <- readRDS(url("https://github.com/benmatthewsed/statistical-methods-criminology-slides/raw/master/resources/simd_crime_sim.rds"))
 
+# maybe the overall relationship between deprivation and crime works differently in cities versus other areas?
+# we'll compare SIMD and recorded crime in 'City' local authorities to other local authorities
+
+# (note that we could have used other operationalizations here, such as the Scottish Government's rural/urban classification)
+
+# create our city_la variable based on whether the LA name has 'City' in it
 
 simd <- 
 simd |> 
@@ -27,155 +34,100 @@ simd |>
                            1,
                            0))
 
+# fit our count model, including an interaction between SIMD and city_la
+# so city LAs get their own SIMD trend, and so do non-city LAs
+
+# (without the interaction the trends are forced to be parallel)
+
 
 qp_mod <- 
 glm(crime_integer_sim ~ simd2020_rank * city_la,
     family = "quasipoisson",
     data = simd)
 
+# let's check the coefficients
 
-# see https://www.r-bloggers.com/2012/08/generate-quasi-poisson-distribution-random-variable/
-
-rqpois <- function(n, mu, theta) {
-  rnbinom(n = n, mu = mu, size = mu/(theta-1))
-}
-
-qp_summary <- 
 qp_mod |> 
-  summary()
+  broom::tidy()
 
-qp_summary$dispersion
+# the main effect of city la is not statistically significant at p < 0.05, but
+# the interaction is
+# therefore we keep both
 
-n_sims <- 10
+# we can calculate adjusted predictions to visualize this interaction
 
-draws <- 
-MASS::mvrnorm(
-  n = n_sims,
-  mu = coef(qp_mod),
-  Sigma = vcov(qp_mod)
-)
+# we can do this just using predict(), asking it to give us predictions for all
+# datazone SIMD ranks (1 to 6976), and under the condition of being in a
+# city_la or not
 
-pred_dat <- 
-simd |> 
-  distinct(city_la) |> 
-  mutate(simd2020_rank = 6976 / 2)
+pred_df <- tidyr::crossing(simd2020_rank = seq(1:6976),
+                           city_la = c(0, 1))
 
-
-
-
-predict(qp_mod,
-        newdata = pred_dat)
-
-
-data.frame(fitted = 
-             predict(qp_mod,
-                     newdata = pred_dat)) |> 
-  mutate(pred_count = map_dbl(fitted, ~ rqpois(n = 1, mu = .x, theta = 31.14463)))
-
-
-pred_mean_crime_draws <- function(fit, ...){
-  
-  fits <- 
-  data.frame(fitted = 
-               predict(fit,
-        newdata = pred_dat))
-  
-  fits |> 
-    mutate(pred_count = map_dbl(fitted, ~ rqpois(n = 1, mu = .x, theta = qp_summary$dispersion))) |> 
-    pull(pred_count)
-    
-  
-}
-
-
-pred_mean_crime_draws(qp_mod)  
-  
-pred_mean_crime_resp <- function(fit, ...){
-  
-  predict(fit,
-          newdata = pred_dat,
-          type = "response"
-  )
-  
-}
-
-pred_30_or_more_crime_resp <- function(fit, ...){
-  
-predict(fit,
-          type = "response"
-    )
-  
-}
-
-
-qp_mod |> predict(type = "response")
-
-sims <- clarify::sim(fit = qp_mod,
-                     n = 100)
-
-pred_30_or_more_crime_resp(qp_mod)
-
-sims
-
-sims$sim.coefs
-
-res <- clarify::sim_apply(sim = sims, FUN = pred_30_or_more_crime_resp)
-
-res
-
-plot(res)
-
-
-
-res
-
-res |> 
-  as.data.frame() |> 
-  as_tibble() |> 
-  t() |> 
-  as.data.frame() |> 
-  mutate(simd = row_number()) |> 
-  pivot_longer(cols = -simd) |> 
-  filter(simd == 1) |> 
-  ggplot(aes(x = value))
-
-# they are all identical and i don't know why?
-
-# probability of 5 or more given SIMD?
-
-
-
-clarify::sim_setx(sims, x = list(simd2020_rank = seq(1:6976), city_la = c(1, 0)),
-         verbose = FALSE) |> plot()
-
-
-
-ames <- clarify::sim_ame(sims, var = "city_la", by = ~simd2020_rank,
-                 contrast = "diff")
-
-
-
-
-plot(ames)
-
-ames2 <- clarify::sim_ame(sims, var = "simd2020_rank", by = ~city_la,
-                         contrast = "diff")
-
-plot(ames2)
-
+simd_preds <- 
 predict(
   qp_mod,
-  newdata = data.frame(council_area = c("Shetland Islands", "City of Edinburgh"),
-                       simd2020_rank = rep(6976 / 2, 2))
+  newdata = pred_df,
+  type = "response"
 )
 
+pred_df |> 
+  mutate(pred = simd_preds) |> 
+  ggplot(aes(x = simd2020_rank, y = pred, colour = factor(city_la))) +
+  geom_line()
+
+# the lines are very similar where simd2020_rank is close to zero
+# (which explains why the main effect was not statistically significant)
+# but they diverge as SIMD rank increases
+# deprived non-city neighbourhoods are quite similar to deprived city neighourhoods
+# but in places with low deprivation city neighbourhoods and non-city neighbourhoods
+# seem quite different
 
 
-# using clarify with our own function -------------------------------------
+# this is fine so far, but really we want to assess the uncertainty in these estimates
+
+# we can do this with {clarify}
+
+# first we simulate coefficients from our model
+
+sims <- clarify::sim(qp_mod,
+                     n = 1000) # this is the default number.  try increasing it and see what happens
+
+# we can use sim_setx to make predictions for the outcome (on the outcome scale)
+# across a grid of predictors that we provide
+
+sim_res <- sim_setx(sims, x = list(simd2020_rank = seq(1:6976), city_la = c(1, 0)),
+         verbose = TRUE) # verbose = TRUE gives a status bar showing how long the simulation is taking
+
+plot(sim_res)
+
+# the solid lines are the same as those we calculated by hand, but we now get error bands
+# showing the 95% confidence intervals
+# these bands overlap at high levels of deprivation, showing that - based on our model - 
+# it looks like city and non-city areas that are very deprived have similar crime rates
 
 
-library(clarify)
 
+
+# practical ---------------------------------------------------------------
+
+# based on the description of the SIMD crime data at
+# see pages 58-63 at https://www.gov.scot/binaries/content/documents/govscot/publications/statistics/2020/09/simd-2020-technical-notes/documents/simd-2020-technical-notes/simd-2020-technical-notes/govscot%3Adocument/SIMD%2B2020%2Btechnical%2Bnotes.pdf
+# and the discussion in sessions three and four
+# write a brief description of the figure produced by the code above, and reflect on the value
+# of the prediction and simulation approach to translating the coefficients
+
+# what are the benefits? what are the limitations?
+
+
+
+# going further: using clarify with our own function -------------------------------------
+
+# in the last example we used a built in function from clarify
+# now we'll recreate the example from the lectures of calculating
+# victimization inequality
+
+
+# load in the data
 
 
 dat <-
@@ -186,33 +138,38 @@ dat <-
     0.197, "2020", "men", 15505,
     0.189, "2020", "women", 18230
   )
+
+
 # calculate the number of victims
 dat <-
   dat |> 
   mutate(vict = as.integer(n * prev))
 
 
+# define our victim divide function
 
 victim_divide <- function(base_y1, base_y2){
   ((base_y2 - 1) - (base_y1 - 1)) / (base_y1 - 1)
 }
 
+# fit a model to calculate the odds of being a victim for men and women,
+# with an interaction with year
 
+# for clarify we need the results to be part of the same model - in the example in the slides
+# we fit two seperate models, to dollow the format of Hunter and Tseloni (2016)
 
 mod1 <- glm(cbind(vict, n - vict) ~ fct_rev(sex) * year,
             family = "binomial",
             data = dat)
 
-n_sims <- 1000
-
 s <- sim(mod1,
-         n = n_sims)
+         n = 1000)
 
-# for clarify we need the results to be part of the same model - in the example in the slides
-# we fit two seperate models, to dollow the format of Hunter and Tseloni (2016)
 
+# this function is quite complicated, but it's just extracting the right coefficients
 
 sim_victim_divide <- function(coefs) {
+  
   men_2015 <- unname(coefs["fct_rev(sex)men"])
   men_interact <- unname(coefs["fct_rev(sex)men:year2020"])
   
@@ -225,6 +182,10 @@ sim_victim_divide <- function(coefs) {
   
 }
 
+# test on our model
+
+sim_victim_divide(coef(mod1))
+
 
 est2 <- sim_apply(s, 
                   FUN = sim_victim_divide)
@@ -233,3 +194,20 @@ tibble(x = as.vector(est2)) |>
   reframe(vds = quantile(x, c(0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.975, 0.99)),
           vals = c(0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.975, 0.99))
 
+# as previously, the 97.5 percenticle is above 0, suggesting that the change in the VD over time
+# is not statistically significant
+
+
+# note that the median VD from our simulations is not identical to the result we calculated
+# from the original model coefficients
+# this is called monte-carlo error - and as a result, the point estimates from the original model
+# are preferable to the median of the simulations
+# see Rainey, C. (2023). A careful consideration of CLARIFY: Simulation-induced bias in point estimates of quantities of interest. Political Science Research and Methods, 1–10. https://doi.org/10.1017/psrm.2023.8
+
+
+
+# going even further ------------------------------------------------------
+
+# read the clarify documentation at https://iqss.github.io/clarify/
+# fit another model to the SIMD data and desribe the results using some of clarify's
+# functionality
